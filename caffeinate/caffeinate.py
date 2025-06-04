3471486b983b50458b8a2a785ac51714754bb67e
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2025-06-04 05:56:47 -0500

    [UPDATE] Cli: Use timedelta for time arguments
    This also extracts argument parsing into a
    function.

--- caffeinate/caffeinate.py
+++ caffeinate/caffeinate.py
@@ -1,11 +1,14 @@
-"""A module of classes that keep the computer awake for a period of time."""
+"""
+A module of classes that keep the computer awake for a period of time.
+"""
+
 import argparse
 import re
 import signal
 import subprocess
 import sys
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 from pathlib import Path
 from threading import Thread
 from typing import Tuple, Union
@@ -13,81 +16,24 @@ from typing import Tuple, Union
 from pynput.keyboard import Controller, Key, Listener
 from Xlib import display
 
+from . import __version__ as VERSION
+
 APP = str(Path(sys.argv[0]).stem)
 
-class cf_time:
-    @classmethod
-    def convert_time(cls, tm: str, *, split: bool = False) -> Union[int, Tuple[int, int, int]]:
-        hours = minutes = seconds = 0
+def timestring(string: str) -> timedelta:
+    if (not re.fullmatch(r'(?:[0-9]{1,2}:)*[0-9]{2}', string)):
+        raise TypeError(f"invalid time string {string!r}")
 
-        # HH:MM:SS
-        m = re.match(r'(\d{1,2}):(\d{2}):(\d{2})', tm)
-        if m:
-            hours, minutes, seconds = m.group(1, 2, 3)
-            if split:
-                return int(hours), int(minutes), int(seconds)
-            hours = convert_time(hours + 'h')
-            minutes = cls.convert_time(minutes + 'm')
-            return hours + minutes + seconds
+    match string.split(":"):
+        case (hs, ms, ss):
+            return timedelta(hours=int(hs), minutes=int(ms), seconds=int(ss))
+        case (ms, ss):
+            return timedelta(minutes=int(ms), seconds=int(ss))
+        case lst:
+            if len(lst) != 1:
+                raise ValueError(f"time string {string!r} has too many fields")
 
-        # MM:SS
-        m = re.match(r'(\d{1,2}):(\d{2})', tm)
-        if m:
-            minutes, seconds = m.group(1, 2)
-            if split:
-                return 0, int(minutes), int(seconds)
-            minutes = cls.convert_time(minutes + 'm')
-            return minutes + int(seconds)
-
-        # [S]S[x], x = m|h|s
-        m = re.match(r'([1-9][0-9]*)([mhs]?)', tm)
-        if not m:
-            raise ValueError(f"invalid time: '{tm}'")
-
-        number = int(m[1])
-        unit = m[2] or 's'
-        multiplier = {'h': 3600, 'm': 60, 's': 1}[unit]
-
-        if split:
-            minutes = hours = 0
-            seconds = number * multiplier
-
-            # Extract minutes and hours from number of seconds
-            minutes = int(seconds / 60)
-            seconds %= 60
-            hours = int(minutes / 60)
-            minutes %= 60
-
-            return hours, minutes, seconds
-
-        # No split, return total number of seconds
-        return number * multiplier
-
-    def __init__(self, tm: str = ''):
-        """Initialize a CLASS object."""
-        self.__hours, self.__minutes, self.__seconds = self.convert_time(tm, split=True)
-
-        # Format time string
-        tm = "%d"
-        fields = self.__seconds
-        if self.__minutes:
-            tm = "%d:%02d"
-            fields = (self.__minutes, self.__seconds)
-        if self.__hours:
-            tm = "%d:%02d:%02d"
-            fields = (self.__hours, self.__minutes, self.__seconds)
-        self.__srep = tm % fields
-
-    def __str__(self) -> str:
-        return self.__srep
-
-    def __repr__(self) -> str:
-        return f"cf_time(hours={self.__hours}, minutes={self.__minutes}, seconds={self.__seconds})"
-
-    @property
-    def seconds(self) -> int:
-        """The number of seconds total."""
-        return self.__seconds + self.__minutes * 60 + self.__hours * 60
+    return timedelta(seconds=int(lst[0]))
 
 class Caffeinate:
     def __init__(self):
@@ -167,12 +113,7 @@ def make_unmapped_window(wm_name) -> display.Display:
 def die(err):
     sys.exit(APP + ': ' + err)
 
-def run():
-    """
-    Listens for user's keystrokes, if none for given time, shift is pressed to keep the computer awake
-    Args:
-        --time ([int]): [default time interval for keypress to take place]
-    """
+def parse_arguments():
     parser = argparse.ArgumentParser(prog=APP)
     subparsers = parser.add_subparsers(help='pass --help to the subcommand for options',
                                        title='subcommands', dest='subcommand', required=True)
@@ -181,6 +122,7 @@ def run():
                                       description='Runs COMMAND and keeps computer awake until it finishes.')
     parser_do.add_argument('COMMAND', help='command to run')
     parser_do.add_argument('ARGUMENT', nargs='*', default=None, help='arguments to command')
+
     # Subcommand 'loop'
     parser_loop = subparsers.add_parser('loop', help='keep the computer awake until the user stops it',
                                         description="Run this command to prevent the computer from falling asleep. "
@@ -188,16 +130,25 @@ def run():
     # Subcommand 'sleep'
     parser_sleep = subparsers.add_parser('sleep', help='keep the computer awake for a given period of time',
                                          description='This command keeps the computer awake for given amount of time.')
-    parser_sleep.add_argument('TIME', default='1:30', type=cf_time,
+    parser_sleep.add_argument('TIME', default='1:30', type=timestring,
                               help='sleep for a certain amoutn of time; can be number with optional h/m suffix, '
                               'or a string in the [[HH:]M]M:SS format')
     # Global options
     parser.add_argument('-V', '--version', action='version', version=f"%(prog)s {VERSION}",
                         help='print the program version and exit')
-    parser.add_argument('-t', '--time', default='1:30', type=cf_time,
+    parser.add_argument('-t', '--time', default='1:30', type=timestring,
                         help='activate after certain time either; can be number with optional h/m suffix, '
                         'or a string in the [[HH:]M]M:SS format')
-    args = parser.parse_args()
+
+    return parser.parse_args()
+
+def run():
+    """
+    Listens for user's keystrokes, if none for given time, shift is pressed to keep the computer awake
+    Args:
+        --time ([int]): [default time interval for keypress to take place]
+    """
+    args = parse_arguments()
 
     match args.subcommand:
         case 'loop':
